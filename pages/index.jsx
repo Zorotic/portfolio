399126e978cdbb63fa525733d4e10c24e1679e45
Zorotic <zorotic@captchabot.xyz>
2021-08-12 20:22:36 -0400

    idk

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,22 +10,13 @@ export default function Home() {
 			<div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
 				<Heading as='h1' id="heading" sx={{ textAlign: 'center' }}>Zorotic</Heading>
 				<h4 id="heading2" as='h4' sx={{ textAlign: 'center', marginTop: 2}}>
-				{"<"} 19, Hobbyist interested in making things scal. {"/>"}
+				{"<"} 19, Hobbyist interested in making things scale. {"/>"}
 				</h4>
 				<Header/>
 				<h4 className="about" as='h4' sx={{ textAlign: 'center', marginTop: 10, color: 0xD91895 }}>
 				CTO, Ex-Software Engineer<br/>
 				CTO at <a className="about-links" href="https://plexidev.org/" target="_blank">Plexi Development.</a><br/>
 				</h4>
-				<Heading id="projects-heading" as='h3' sx={{
-					paddingTop: 50,
-					textAlign: 'center'
-				}}>
-				{/* {"<"} Contact Me {"/>"} */}
-				</Heading>
-				{/* <Container className="additional">
-					<Additional className="item" title="Email" description="zorotic@captchabot.xyz" url="mailto:zorotic@captchabot.xyz"/>
-				</Container> */}
 				<Footer/>
 			</div>
 		</div>
