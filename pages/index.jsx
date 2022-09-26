80b1b99b668608213195b5b0b0c8212a80036878
Zorotic <zorotic@captchabot.xyz>
2022-09-26 19:39:49 -0400

    update short bio

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
 			<div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
 				<Heading as='h1' id="heading" sx={{ textAlign: 'center' }}>Zorotic</Heading>
 				<h4 id="heading2" as='h4' sx={{ textAlign: 'center', marginTop: 2}}>
-				{"<"} 19, Hobbyist interested in making things scale. {"/>"}
+				{"<"} 21 years old, He/Him, Hobbyist interested in making scalable/realtime applications. {"/>"}
 				</h4>
 				<Header/>
 				{/* <h4 className="about" as='h4' sx={{ textAlign: 'center', marginTop: 10, color: 0xD91895 }}>
