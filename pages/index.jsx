5f3de01036d04077a96c807f2f0b2a62a50c7d3d
Zorotic <zorotic@captchabot.xyz>
2022-09-26 19:42:13 -0400

    removed unused code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import { Heading } from 'theme-ui';
-import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function Home() {
@@ -9,11 +8,6 @@ export default function Home() {
 				<h4 id="heading2" as='h4' sx={{ textAlign: 'center', marginTop: 2}}>
 				{"<"} 21 years old, He/Him, Hobbyist interested in making scalable/realtime applications. {"/>"}
 				</h4>
-				<Header/>
-				{/* <h4 className="about" as='h4' sx={{ textAlign: 'center', marginTop: 10, color: 0xD91895 }}>
-				CTO, Ex-Software Engineer<br/>
-				CTO at <a className="about-links" href="https://plexidev.org/" target="_blank">Plexi Development.</a><br/>
-				</h4> */}
 				<br/>
 				<Footer/>
 			</div>
