bf5b2ce96f50e5c4e89f93e246285346b22709ae
Zorotic <swftapocalypse1@gmail.com>
2023-12-06 18:37:04 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 			<div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
 				<Heading as='h1' id="heading" sx={{ textAlign: 'center' }}>Zorotic</Heading>
 				<h4 id="heading2" as='h4' sx={{ textAlign: 'center', marginTop: 2}}>
-				{"<"} 21 years old, He/Him, Hobbyist interested in making scalable/realtime applications. {"/>"}
+				{"<"} 22 years old, He/Him, Hobbyist interested in making scalable/realtime applications. {"/>"}
 				</h4>
 				<br/>
 				<Footer/>
